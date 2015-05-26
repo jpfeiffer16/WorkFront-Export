2d6935cb71ba11c9297eb497cee5dab84d5aff46
Joe <Pfeiffer>
2015-05-26 13:52:19 -0600

    Working on it again

--- src/page_action/exporter-ui.js
+++ src/page_action/exporter-ui.js
@@ -32,7 +32,7 @@ var init = function() {
 		newNode.innerText = this.title;
 		reportList.appendChild(newNode);
 	});
-	document.getElementById('btnSave').addEventListener(function(e) {
+	document.getElementById('btnSave').addEventListener('click', function(e) {
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			chrome.tabs.sendMessage(tabs[0].id, {eventType: "button"}, function(response) {
 			console.log(response.recieved);
